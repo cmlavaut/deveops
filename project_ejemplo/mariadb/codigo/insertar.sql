d0a6923c47681741937af2a14c334f1416551db8
Camila <cmlavaut96@gmail.com>
2025-03-03 12:11:28 -0600

    Updated 030325

--- project_ejemplo/mariadb/codigo/insertar.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `login` (`nombre`, `email`, `username`, `password`)
-VALUES (%s,%s,%s,%s,%s);
--- terraform-example/prueba/main.tf
+++
@@ -1,9 +0,0 @@
-provider "local" {
-  
-}
-
-resource "local_file" "hello"{
-        content = var.text_file
-        filename = "prueba.txt"
-
-}
